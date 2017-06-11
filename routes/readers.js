9b4f01855caed889b52c940b14ca9f21a707e518
Davidcopperfiled <davidueux@gmail.com>
2017-06-10 22:10:00 -0500

    readers support update/create/select

--- routes/readers.js
+++ routes/readers.js
@@ -45,30 +45,8 @@ var Reader = sequelize.define('reader', {
     timestamps: false
 });
 
-var now = Date.now();
 
-Reader.create({
-    Readerid: 4000023,
-    username: 'Odie',
-    firstname: 'Oo',
-    lastname: 'La',
-    middlename: '',
-    password: 'Odie',
-    church: 'CCC',
-    groups: 'Pingan1',
-    email: 'e@g.c',
-    phonenumber: '111',
-    memo: '2',
-    gender: false,
-    birth: '2008-08-08',
-    createdAt: now,
-    updatedAt: now,
-    version: 0
-}).then(function(p) {
-    console.log('created.' + JSON.stringify(p));
-}).catch(function(err) {
-    console.log('failed: ' + err);
-});
+
 
 
 // (async(function testingAsyncAwait() {
@@ -128,15 +106,15 @@ router.get('/', function(req, res) {
     // }))();
 
     try {
-        Reader.findAll().then(function(readers){
-        	console.log(JSON.stringify(readers));
-        	res.send(readers);
+        Reader.findAll().then(function(readers) {
+            console.log(JSON.stringify(readers));
+            res.send(readers);
         });
         // console.log(`find ${pets.length} pets:`);
         // for (let r of readers) {
         //     console.log(JSON.stringify(r));
         // }
-        
+
     } catch (err) {
         console.log(err);
     }
@@ -155,47 +133,100 @@ router.get('/', function(req, res) {
 });
 
 
-//     (async() => {
-//         var readers = await Reader.findAll();
-//         console.log(`find ${pets.length} pets:`);
-//         for (let p of readers) {
-//             console.log(JSON.stringify(p));
-//         }
-//     })();
-// });
-
-
-router.post('/', function(req, res) {
+router.put('/', function(req, res) {
     // console.log('body:', req.body);
+
+    var now = Date.now();
+    var username = req.body.username;
     var firstname = req.body.firstname;
     var lastname = req.body.lastname;
     var middlename = req.body.middlename;
+    var gender = req.body.gender;
+    var birth = req.body.birth;
     var church = req.body.church;
     var groups = req.body.groups;
     var email = req.body.email;
     var phonenumber = req.body.phonenumber;
     var memo = req.body.memo;
 
-    var connection = mysql.createConnection(dbconfig.connection);
-    var reader = {
+    try {
+        Reader.findAll({
+            where: {
+                username: username
+            }
+        }).then(function(users) {
+            console.log('users are:', JSON.stringify(users));
+            // r.version++;
+            users.forEach(function(u) {
+				u.version++;
+                    // console.log('fff----',firstname,u.version++);
+                    u.firstname=firstname
+                     u.lastname=lastname,
+                    u.middlename=middlename,
+                    u.church=church,
+                    u.groups=groups,
+                    u.email=email,
+                    u.phonenumber=phonenumber,
+                    u.memo=memo,
+                    u.gender=gender,
+                    u.birth=birth,
+                    u.updatedAt=now
+                u.save();
+                })
+               
+            res.send('Updated!');
+        });
 
-        firstname: firstname,
-        lastname: lastname,
-        middlename: middlename,
-        church: church,
-        groups: groups,
-        email: email,
-        phonenumber: phonenumber,
-        memo: memo
-    };
-    var query = connection.query('INSERT INTO test.readers SET ?', reader, function(error, results, fields) {
-        if (error) throw error;
-        // Neat!
+    } catch (err) {
+        console.log(err);
+    }
+
+    // });
+});
+
+/*register new reader*/
+router.post('/', function(req, res) {
+    // console.log('body:', req.body);
+
+    var now = Date.now();
+    var username = req.body.username;
+    var password = req.body.password;
+
+
+    Reader.create({
+
+        username: username,
+        password: password,
+        createdAt: now,
+        version: 0
+    }).then(function(p) {
+        console.log('created.' + JSON.stringify(p));
+    }).catch(function(err) {
+        console.log('failed: ' + err);
     });
-    console.log(query.sql); // INSERT INTO posts SET `id` = 1, `title` = 'Hello MySQL'
+
+    /*
+        var connection = mysql.createConnection(dbconfig.connection);
+        var reader = {
+
+            firstname: firstname,
+            lastname: lastname,
+            middlename: middlename,
+            church: church,
+            groups: groups,
+            email: email,
+            phonenumber: phonenumber,
+            memo: memo
+        };
+        var query = connection.query('INSERT INTO test.readers SET ?', reader, function(error, results, fields) {
+            if (error) throw error;
+            // Neat!
+        });
+        console.log(query.sql); // INSERT INTO posts SET `id` = 1, `title` = 'Hello MySQL'
 
 
-    connection.end();
+        connection.end();
+        */
 
     res.send('Insert new user ok!');
 });
