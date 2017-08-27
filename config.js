9df1b5b3b9a627ae50554dd933fbf7e9d84b11e7
qid14 <davidueux@gmail.com>
2017-08-27 00:50:44 -0500

    Update database condif

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ module.exports = {
     'connection': {
         'host': 'localhost',
         'user': 'root',
-        'password': 'temp1234',
+        'password': 'Passinglight!123',
 
     },
-    'database': 'test',
+    'database': 'passinglight',
     'secretToken':'FF755A66CC6D45675289F6B1386A7'
 }
